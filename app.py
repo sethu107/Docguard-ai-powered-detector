a2a651deac7138160ac16499f6b8675ac22c591e
SRIRAM-E11 <sriramelangovan11@gmail.com>
2026-05-19 01:08:13 +0530

    port added

--- app.py
+++ app.py
@@ -49,6 +49,6 @@ def post():
 
     return render_template("index.html", results=results_array)
 
-
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
